591a942081b69602fbf59d389f0b2fceb5b2386b
Ronalde Gonçalves <ronalderota@gmail.com>
2024-04-23 13:18:55 -0300

    upgrade2

--- desafio1.js
+++ desafio1.js
@@ -1,26 +1,35 @@
+// const readline = require('readline').createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+//   });
+  
+//   readline.question('fala seu Herói? ', (Herói) => {
+//     console.log(`Seu Herói é: ${heroi}`);
+//     readline.close();
+//   });
 let heroi = 'Mec'
-let pontos = 1
+let pontos = 8888
 
 if (pontos <= 1000) {
     console.log('O Herói de nome: ' + heroi + ', está no nível de ' + 'Ferro')
 }
 
-else if (pontos >= 1001 || pontos <= 2000){
+else if (pontos >= 1001 && pontos < 2000){
     console.log('O Herói de nome: ' + heroi + ', está no nível de ' + 'Bronse')
 }
-else if (pontos >= 2001  || pontos <= 5000){
+else if (pontos >= 2001  && pontos <= 5000){
     console.log('O Herói de nome: ' + heroi + ', está no nível de ' + 'Prata')
 }
-else if (pontos >= 5001 || pontos <= 7000){
+else if (pontos >= 5001 && pontos <= 7000){
     console.log('O Herói de nome: ' + heroi + ', está no nível de ' + 'Ouro')
 }
-else if (pontos >= 7001 || pontos <= 8000){
+else if (pontos >= 7001 && pontos <= 8000){
     console.log('O Herói de nome: ' + heroi + ', está no nível de ' + 'Platina')
 }
-else if (pontos >= 8001 || pontos <= 9000){
+else if (pontos >= 8001 && pontos <= 9000){
     console.log('O Herói de nome: ' + heroi + ', está no nível de ' + 'Ascendente')
 }
-else if (pontos >= 9001 || pontos <= 1000){
+else if (pontos >= 9001 && pontos <= 1000){
     console.log('O Herói de nome: ' + heroi + ', está no nível de ' + 'Imortal')
 }
 else if (pontos >= 10001){
